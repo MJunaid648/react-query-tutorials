5199c9b489248a396067bed9a9bc1735c76a908e
Muhammad Junaid <648mjunaid@gmail.com>
2024-09-12 13:49:39 +0500

    refetching flags & event based refetching

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -6,23 +6,32 @@ const fetchSuperHeroes = () => {
 };
 
 const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
-    { cacheTime: 5000 }
+    {
+      enabled: false,
+      // staleTime:40000,
+      // cacheTime:120000,
+      // refetchOnReconnect:true,
+      // refetchOnWindowFocus:true,
+      // refetchOnMount:true,
+      // refetchInterval:3000,
+      // refetchIntervalInBackground:true,
+    }
   );
   console.log("Loading:", isLoading, "     Fetching:", isFetching);
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
   if (isError) return <h2>{error.message}</h2>;
-
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
       {data?.data.map((hero) => (
         <div key={hero.name}>{hero.name}</div>
       ))}
+      <button onClick={refetch}>Refetch Data</button>
     </>
   );
 };
